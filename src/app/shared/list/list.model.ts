70a2fcc142dbe7d56039e47be1b5daf382ea2425
snikcr <sofia.c.rodriguez@gmail.com>
2018-03-31 01:26:45 +0200

    Remove list component

--- src/app/shared/list/list.model.ts
+++
@@ -1,4 +0,0 @@
-export interface Product {
-    id: number;
-    description: string;
-}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { ListComponent } from './list/list.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DataService } from './data.service';
 import { LabelPipe } from './label/label.pipe';
 
 @NgModule({
-    declarations: [ HeaderComponent, ListComponent, LabelPipe ],
+    declarations: [ HeaderComponent, LabelPipe ],
     imports: [ CommonModule, FormsModule ],
-    exports: [ HeaderComponent, ListComponent, CommonModule, FormsModule ],
+    exports: [ HeaderComponent, CommonModule, FormsModule ],
     providers: [ DataService ]
 })
 export class SharedModule {}
