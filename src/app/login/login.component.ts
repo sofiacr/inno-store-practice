e4283451069f0460da2f9e30724fb052815070da
snikcr <sofia.c.rodriguez@gmail.com>
2018-03-31 00:09:50 +0200

    Add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../shared/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  constructor(private authService: AuthService) {
+  }
+
+  login() {
+    this.authService.login();
+    console.log('Navigate to products list');
+  }
+}
