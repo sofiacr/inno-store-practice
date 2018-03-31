7f7186ddc6d68a5efa2436f4076e7b4c4c6ca9ae
snikcr <sofia.c.rodriguez@gmail.com>
2018-03-31 23:55:02 +0200

    Add page not found component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DataService } from './data.service';
 import { LabelPipe } from './label/label.pipe';
+import { NotFoundComponent } from './not-found/not.found.component';
 
 @NgModule({
-    declarations: [ HeaderComponent, LabelPipe ],
+    declarations: [ HeaderComponent, LabelPipe, NotFoundComponent ],
     imports: [ CommonModule, FormsModule ],
-    exports: [ HeaderComponent, CommonModule, FormsModule ],
+    exports: [ HeaderComponent, CommonModule, FormsModule, NotFoundComponent ],
     providers: [ DataService ]
 })
 export class SharedModule {}
