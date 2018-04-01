386304ba0acfd7e8b7bb89f6a8f2421c780ca781
sofia rodriguez <sofia.rodriguez@innocv.com>
2018-04-01 02:06:42 +0200

    Change shop main component to make it root for shop routing

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,25 +1,9 @@
-import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
-import { Shop } from './shop';
-import { DataService } from '../shared/data.service';
 
 @Component({
     selector: 'app-shop',
     templateUrl: './shop.component.html',
     styleUrls: ['./shop.component.css']
 })
-export class ShopComponent implements OnInit {
-
-    private static TARGET = 'shop';
-    private shops: Shop[];
-
-    constructor(
-        private dataService: DataService<Shop>
-    ) {}
-
-    ngOnInit(): void {
-        this.dataService.list(ShopComponent.TARGET)
-            .subscribe(shops => this.shops = shops);
-    }
-
+export class ShopComponent {
 }
