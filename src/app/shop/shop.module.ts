43a0830ae81e44f7645bea3cc76ec1f4849af245
sofia rodriguez <sofia.rodriguez@innocv.com>
2018-04-01 02:08:30 +0200

    Configure shop components, service and routing

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ShopService } from './shop.service';
+import { ShopListComponent } from './shop.list.component';
+import { ShopRoutingModule } from './shop-routing.module';
+import { ShopDetailComponent } from './shop.detail.component';
 
 @NgModule({
-    imports: [ CommonModule ],
-    declarations: [ ShopComponent ],
-    exports: [ ShopComponent ]
+    imports: [ CommonModule, ShopRoutingModule ],
+    declarations: [ ShopComponent, ShopListComponent, ShopDetailComponent ],
+    exports: [ ShopComponent ],
+    providers: [ ShopService ]
 })
 export class ShopModule {
 
