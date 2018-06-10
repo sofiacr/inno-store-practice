f3dc82d2231acb6266e7868b0345c499773c729b
sofia rodriguez <sofia.rodriguez@innocv.com>
2018-06-10 21:54:16 +0200

    Add routing for new product

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -4,11 +4,13 @@ import { ProductComponent } from './product.component';
 import { ProductListComponent } from './product.list.component';
 import { ProductDetailComponent } from './product.detail.component';
 import { AuthGuard } from '../auth-guard.service';
+import { AddProductComponent } from './add-product/add-product.component';
 
 const productRoutes: Routes = [
     { path: 'products', component: ProductComponent, children: [
         { path: '', component: ProductListComponent, canActivate: [ AuthGuard ] },
-        { path: ':id', component: ProductDetailComponent, canActivate: [ AuthGuard ]}
+        { path: ':id', component: ProductDetailComponent, canActivate: [ AuthGuard ]},
+        { path: 'add', component: AddProductComponent, canActivate: [ AuthGuard ]}
     ], canActivate: [ AuthGuard ] }
 ];
 
